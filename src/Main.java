c9aaa1f0061c9ad9c711fdddbc668e2f6af4b5ca
Ikemike <cpu.ikemike@gmail.com>
2016-01-31 17:07:38 -0600

    Test Commit.

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import org.apache.log4j.*;
+/**
+ * Created by Ike on 1/31/2016.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        UnitTest myTest = new UnitTest();
+        System.out.println("Test");
+        myTest.myUnitTest();
+    }
+}
---
+++ src/UnitTest.java
@@ -0,0 +1,12 @@
+import org.apache.log4j.*;
+/**
+ * Created by Ike on 1/31/2016.
+ */
+public class UnitTest {
+
+    private final Logger logger = Logger.getLogger(this.getClass());
+    public void myUnitTest() {
+        logger.info("Here's a test message I want logged.");
+
+    }
+}
